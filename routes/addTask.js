5459e50b8e69e9da3b22c85b33bca39cd4170343
AshishResolute <anshugourh21@gmail.com>
2025-10-11 16:04:35 +0530

    Add get /by-group/CompletedTasks to get the summary of completed tasks

--- routes/addTask.js
+++ routes/addTask.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/',async(req,res)=>{
     try{
         let {task_Id,task} = req.body;
-        await db.query('insert into tasks values(?,?)',[task_Id,task]);
+        await db.query('insert into tasks (task_Id,task) values(?,?)',[task_Id,task]);
         res.status(200).json(`Task ${task} with Id ${task_Id} added`);
     }
     catch(err)
--- routes/task.js
+++ routes/task.js
@@ -26,5 +26,29 @@ router.put('/:id',async(req,res)=>{
         res.status(500).json({Error:'DataBase Error',err:err.message});
     }
 })
-
+router.put('/by-task/:task',async(req,res)=>{
+    try{
+        let task = req.params.task;
+        console.log(task)
+        if(!task) return res.status(400).json(`Enter a task`);
+        let {updateStatus} = req.body;
+        await db.query('update tasks set status=? where task=?',[updateStatus,task])
+        res.status(200).json(`Task Status Updated to ${updateStatus}`)
+    }
+    catch(err)
+    {
+        res.status(500).json({Error:`DataBase error`,err:err.message});
+    }
+})
+router.get('/by-group/CompletedTasks',async(req,res)=>{
+    try{
+        const [tasks] = await db.query('select status,count(*) as count_task from tasks group by status having status="Done"');
+        if(!tasks.length) return res.json(200).json({message:'No Tasks Found'});
+        res.json({"CompletedTasks":tasks[0].count_task});
+    }
+    catch(err)
+    {
+        res.json({Error:`DataBase error`,err:err.message});
+    }
+})
 export default router;
